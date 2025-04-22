52447f408c57f92fa82a6f4d7dbf83fc1f7b602c
adsonfly <syedsalman186org@gmail.com>
2025-04-22 09:45:30 +0530

    updated the style of drawer navigation

--- src/navigation/Sidebar.tsx
+++ src/navigation/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Platform} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from '../features/orders/screens/HomeScreen';
 import WalletScreen from '../features/financial/screens/WalletScreen';
@@ -8,6 +9,7 @@ import OrderHistory from '../features/orders/screens/OrderHistory';
 import PrivacyPolicyScreen from '../features/common/screens/PrivacyPolicyScreen';
 import TermsConditionsScreen from '../features/common/screens/TermsConditionsScreen';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import CustomDrawerContent from '../components/navigation/CustomDrawerContent';
 
 export type DrawerParamList = {
   Home: undefined;
@@ -17,33 +19,83 @@ export type DrawerParamList = {
   OrderHistory: undefined;
   PrivacyPolicy: undefined;
   TermsConditions: undefined;
+  Authentication: undefined;
 };
 
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
 const Sidebar: React.FC = () => {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerActiveBackgroundColor: '#f0e6ff',
+        drawerActiveTintColor: '#340e5c',
+        drawerInactiveTintColor: '#555',
+        drawerLabelStyle: {
+          marginLeft: 0,
+          fontFamily: Platform.OS === 'ios' ? 'System' : 'Roboto',
+          fontSize: 15,
+          fontWeight: '500',
+        },
+        drawerItemStyle: {
+          borderRadius: 8,
+          paddingHorizontal: 5,
+          marginHorizontal: 6,
+          height: 50,
+        },
+        drawerStyle: {
+          width: 280,
+          backgroundColor: '#fff',
+        },
+      }}>
       <Drawer.Screen
         name="Home"
         component={HomeScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => <Icon name="home" size={24} color={color} />,
+        }}
       />
       <Drawer.Screen
         name="Wallet"
         component={WalletScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          drawerIcon: ({color}) => (
+            <Icon name="account-balance-wallet" size={24} color={color} />
+          ),
+        }}
       />
       <Drawer.Screen
         name="StoreManagement"
         component={StoreManagement}
-        // Hide default header, use custom Header
+        options={{
+          headerShown: false,
+          title: 'Store Management',
+          drawerIcon: ({color}) => (
+            <Icon name="store" size={24} color={color} />
+          ),
+        }}
       />
-      <Drawer.Screen name="Help" component={HelpScreen} />
+      <Drawer.Screen
+        name="Help"
+        component={HelpScreen}
+        options={{
+          drawerIcon: ({color}) => <Icon name="help" size={24} color={color} />,
+        }}
+      />
       <Drawer.Screen
         name="OrderHistory"
         component={OrderHistory}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          title: 'Order History',
+          drawerIcon: ({color}) => (
+            <Icon name="history" size={24} color={color} />
+          ),
+        }}
       />
       <Drawer.Screen
         name="PrivacyPolicy"
